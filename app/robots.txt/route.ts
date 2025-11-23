d08a0ed05f118011bfe1eb5ddb7796a777f31e71
Arshad021 <hafizarshadali021@gmail.com>
2025-11-23 16:18:56 +0500

    Initial commit

--- app/robots.txt/route.ts
+++
@@ -1,4 +0,0 @@
-export async function GET() {
-  const body = `User-agent: *\nAllow: /\nSitemap: ${process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000'}/sitemap.xml`
-  return new Response(body, { headers: { 'Content-Type': 'text/plain' }})
-}
--- types/vercel-speed-insights.d.ts
+++
@@ -1,8 +0,0 @@
-declare module '@vercel/speed-insights/next' {
-    export const SpeedInsights: any;
-  }
-  
-  declare module '@vercel/analytics/react' {
-    export const Analytics: any;
-  }
-  
